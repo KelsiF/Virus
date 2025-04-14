65777314a3ebac90190d89f95ba492d97524d8e3
Kelsi <superigromen2018@gmail.com>
2025-04-14 15:12:06 +0300

    v. 1.0

---
+++ scripts/player.gd
@@ -0,0 +1,34 @@
+extends CharacterBody2D
+
+var speed = 300
+var speedMultiplier = 1.0
+
+
+func _ready():
+	pass
+
+func _physics_process(delta):
+	player_falling(delta)
+	player_run(delta)
+	player_jump(delta)
+	
+	move_and_slide()
+
+func player_falling(delta):
+	if !is_on_floor():
+		velocity.y += 1000 * delta
+		
+
+@warning_ignore("unused_parameter")
+func player_run(delta):
+	var direction = Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * speed*speedMultiplier
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed*speedMultiplier)
+
+@warning_ignore("unused_parameter")
+func player_jump(delta):
+	if Input.is_action_just_pressed("jump"):
+		if is_on_floor():
+			velocity.y = -400
